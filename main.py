84217d8b4c4993ba5bbca82c31df036fc7256dbd
georgebrnv <barinov.egor2013@gmail.com>
2024-02-09 17:26:58 -0500

    Add GitHub Project Boxes functionality

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, abort, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
+import requests
+
+# Git REST API setup
+GIT_USER = 'georgebrnv'
+URL = f'https://api.github.com/users/{GIT_USER}/repos'
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "egor.barinov123"
@@ -15,8 +21,12 @@ def inject_current_year():
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    current_year = datetime.today().year
-    return render_template('index.html')
+    git_response = requests.get(url=URL)
+    data = git_response.json()
+    sorted_data = sorted(data, key=lambda x: x["updated_at"], reverse=True)[:6]
+    print(sorted_data)
+
+    return render_template('index.html', git_projects_data=sorted_data)
 
 
 @app.route('/resume', methods=['GET', 'POST'])
