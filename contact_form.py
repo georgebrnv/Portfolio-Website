5cdc3b0f6afa9ca5f6c9ad108eeeb79c8ec086cb
georgebrnv <barinov.egor2013@gmail.com>
2024-02-12 13:04:11 -0500

    Add contact form functionality and form validators

--- contact_form.py
+++ contact_form.py
@@ -1,8 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, URL, Email, ValidationError
+
+
+def name_validation(form, field):
+    if len(field.data) > 50:
+        raise ValidationError('Name must be less than 50 characters.')
+
+
+def email_validation(form, field):
+    if len(field.data) > 100:
+        raise ValidationError('Email must contain less than 100 characters.')
+
+
+def message_validation(form, field):
+    if len(field.data) < 5:
+        raise ValidationError('Message is too short.')
+    elif len(field.data) > 1000:
+        raise ValidationError('Message can NOT be longer than 1000 characters.')
+
 
 class ContactForm(FlaskForm):
-    full_name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email()])
-    message = StringField('Message', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), name_validation])
+    email = StringField('Email Address', validators=[DataRequired(), Email(granular_message=True), email_validation])
+    message = TextAreaField('Message', validators=[DataRequired(), message_validation])
+    submit = SubmitField('Submit')
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, abort, render_template, redirect, url_for
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 import requests
+from contact_form import ContactForm
 
 # Git REST API setup
 GIT_USER = 'georgebrnv'
@@ -23,8 +24,8 @@ def inject_current_year():
 def home():
     git_response = requests.get(url=URL)
     data = git_response.json()
+    # Sort data by "last time updated" and take last 6 projects.
     sorted_data = sorted(data, key=lambda x: x["updated_at"], reverse=True)[:6]
-
     return render_template('index.html', git_projects_data=sorted_data)
 
 
@@ -35,7 +36,31 @@ def resume():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            message = form.message.data
+            # Sent email
+            sent_email_message(name=name, email=email, message=message)
+            # Flash success message
+            flash('Message was successfully sent.', 'success')
+            return redirect(url_for('contact'))
+        else:
+            # Flash error messages from form errors dictionary
+            flash_errors(form)
+    return render_template('contact.html', form=form)
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(f"{error}", "warning")
+
+
+def sent_email_message(name, email, message):
+    print(name, email, message)
 
 
 if __name__ == '__main__':
