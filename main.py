156e84262c73c96c9780a44b1e8449eb73eb4cc8
georgebrnv <barinov.egor2013@gmail.com>
2024-02-14 16:03:02 -0500

    Roll back

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 import requests
-from api.templates.contact_form import ContactForm
+from contact_form import ContactForm
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 import os
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -53,8 +54,26 @@ def contact():
             name = form.name.data
             email = form.email.data
             message = form.message.data
+            # MIMEText template
+            html = f"""\
+                        <html>
+                        <head></head>
+                        <body>
+                        <h2 style="font-weight: bold;">Portfolio Website Contact Form</h2>
+                        <p style="font-size: 20px"><strong>PROVIDED EMAIL:</strong> {email}</p>
+                        <p style="font-size: 20px"><strong>PROVIDED NAME:</strong> {name}</p>
+                        </body>
+                        </html>
+                    """
+            text = message
+            message_mime = MIMEMultipart()
+            message_mime['Subject'] = 'Portfolio Website Contact Form'
+            message_mime['From'] = MY_EMAIL
+            message_mime['To'] = MY_EMAIL
+            message_mime.attach(MIMEText(html, 'html'))
+            message_mime.attach(MIMEText(text, 'plain'))
             # Sent email
-            sent_email_message(name=name, email=email, message=message)
+            sent_email_message(message=message_mime)
             # Flash success message
             flash('Message was successfully sent.', 'success')
             return redirect(url_for('contact'))
@@ -70,29 +89,15 @@ def flash_errors(form):
             flash(f"{error}", "warning")
 
 
-def sent_email_message(name, email, message):
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=GMAIL_APP_PASSWORD)
-        html = f"""\
-                    <html>
-                    <head></head>
-                    <body>
-                    <h2 style="font-weight: bold;">Portfolio Website Contact Form</h2>
-                    <p style="font-size: 20px"><strong>PROVIDED EMAIL:</strong> {email}</p>
-                    <p style="font-size: 20px"><strong>PROVIDED NAME:</strong> {name}</p>
-                    </body>
-                    </html>
-                    """
-        text = message
-        message = MIMEMultipart()
-        message['Subject'] = 'Portfolio Website Contact Form'
-        message['From'] = MY_EMAIL
-        message['To'] = MY_EMAIL
-        message.attach(MIMEText(html, 'html'))
-        message.attach(MIMEText(text, 'plain'))
-
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=message.as_string())
+def sent_email_message(message):
+    try:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=GMAIL_APP_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=message.as_string())
+            print('Email was successfully sent.')
+    except Exception as e:
+        print(f'Error sending email: {e}')
 
 
 if __name__ == '__main__':
